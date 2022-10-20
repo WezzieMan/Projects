28cab5b466a69bcd729ce1d35d1d6b208d82520b
Daniel Florian Marcoci <florian.danutz15@gmail.com>
2022-10-20 17:30:01 +0200

    other updates
    added a btn to reset the page

--- Portfolio/Project4-PlayWithEvents/script/script.js
+++ Portfolio/Project4-PlayWithEvents/script/script.js
@@ -1,9 +1,36 @@
+/* ********** EVENTS FOR THE CONTAINER ********** */
 const gridContainer = document.querySelector(".grid-container");
 
 gridContainer.addEventListener("mouseenter", () => {
     gridContainer.style.outline = "6px solid red";
 });
-
 gridContainer.addEventListener("mouseleave", () => {
     gridContainer.style.outline = "";
+});
+
+/* ********** EVENTS FOR ELEMENTS INSIDE CONTAINER ********** */
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach(cell => {
+  const cellsBackground = cell.style.backgroundColor;
+
+  cell.addEventListener("mouseenter", () => {
+    cell.style.outline = "3px dashed red";
+  });
+  cell.addEventListener("mouseleave", () => {
+    cell.style.outline = ""
+  });
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor != "white" ? 
+    cell.style.backgroundColor = "white" : 
+    cell.style.backgroundColor = cellsBackground;
+  })
+});
+
+/* ********** EVENTS FOR BUTTON ********** */
+const btn = document.querySelector(".confirmBtn")
+
+btn.addEventListener("click", () => {
+  /*This reloads the page*/
+  location.reload();
 });
